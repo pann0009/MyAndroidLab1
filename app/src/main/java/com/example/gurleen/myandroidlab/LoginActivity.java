9f74543b02632169bd38d649bb6c2f9397ef2f05
GurleenPannu <pann0009@algonquinlive.com>
2017-11-14 13:39:24 -0500

    prefrences

--- app/src/main/java/com/example/gurleen/myandroidlab/LoginActivity.java
+++ app/src/main/java/com/example/gurleen/myandroidlab/LoginActivity.java
@@ -2,9 +2,11 @@ package com.example.gurleen.myandroidlab;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -19,12 +21,12 @@ public class LoginActivity extends Activity {
         Log.i(ACTIVITY_NAME, "In onCreate()");
 
         final Button button =findViewById(R.id.button2);
-        final EditText editText = (EditText) findViewById(R.id.editText);
+        //final EditText editText = (EditText) findViewById(R.id.editText);
 
-       String text = editText.getText().toString();
-        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
-        int defaultValue = getResources().getInteger(R.string.saved_high_score_default);
-        long highScore = sharedPref.getInt(getString(R.string.saved_high_score), defaultValue);
+       //String text = editText.getText().toString();
+       // SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        //int defaultValue = getResources().getInteger(R.string.saved_high_score_default);
+        //long highScore = sharedPref.getInt(getString(R.string.saved_high_score), defaultValue);
 
        // SharedPreferences.Editor editor = getSharedPreferences(MY_PRFS_NAME, MODE_PRIVATE).edit();
         //editor.putString(editText.getText().toString(), null);
@@ -33,14 +35,21 @@ public class LoginActivity extends Activity {
         //SharedPreferences sharedPreferences = null;
 //        sharedPreferences.getString("DefaultEmail","email@domain.com");
 
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
 
 
     }
 
-    private Activity getActivity() {
-        return activity;
-    }
+
 
     public void onResume() {
 
