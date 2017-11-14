8c55359f357cf051793837db9f70a4f9405a6aee
GurleenPannu <pann0009@algonquinlive.com>
2017-11-14 14:48:34 -0500

    button listner added

--- app/src/main/java/com/example/gurleen/myandroidlab/MainActivity.java
+++ app/src/main/java/com/example/gurleen/myandroidlab/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.gurleen.myandroidlab;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
     protected static final String ACTIVITY_NAME = "MainActivity";
@@ -11,6 +14,21 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(ACTIVITY_NAME, "In onCreate()");
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ListItemActivity.class);
+                startActivityForResult(intent, 10);
+            }
+        });
+    }
+    public void onActivityResult(int requestCode, int responseCode, Intent data){
+        if(requestCode==10){
+            Log.i(ACTIVITY_NAME,"Returned to StartActivity.onActivityResult");
+        }
+
     }
     public void onResume() {
 
