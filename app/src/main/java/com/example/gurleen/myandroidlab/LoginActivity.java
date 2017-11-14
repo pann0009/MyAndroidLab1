b75c414d3d8fc63b98b3e7e010f4674e73eab919
GurleenPannu <pann0009@algonquinlive.com>
2017-11-14 11:28:09 -0500

    did the all function in the three acitvities

--- app/src/main/java/com/example/gurleen/myandroidlab/LoginActivity.java
+++ app/src/main/java/com/example/gurleen/myandroidlab/LoginActivity.java
@@ -2,12 +2,41 @@ package com.example.gurleen.myandroidlab;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class LoginActivity extends Activity {
-
+    protected static final String ACTIVITY_NAME = "LoginActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+
     }
+    public void onResume() {
+
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+    public void	onStart() {
+
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+    public void onPause() {
+
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+    }
+    public void onStop() {
+
+        super.onStop();
+    }
+    public void onDestroy() {
+
+        super.onDestroy();
+    }
+
+
+
 }
--- app/src/main/java/com/example/gurleen/myandroidlab/MainActivity.java
+++ app/src/main/java/com/example/gurleen/myandroidlab/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.gurleen.myandroidlab;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
-
+    protected static final String ACTIVITY_NAME = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(ACTIVITY_NAME, "In onCreate()");
     }
+    public void onResume() {
+
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+    public void	onStart() {
+
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+    public void onPause() {
+
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+    }
+    public void onStop() {
+
+        super.onStop();
+    }
+    public void onDestroy() {
+
+        super.onDestroy();
+    }
+
+
 }
