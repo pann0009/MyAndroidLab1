fa186f1dd8bee6c5cc952fb74eb100fd49597045
GurleenPannu <pann0009@algonquinlive.com>
2017-11-19 14:35:54 -0500

    Lab04 Partially completed

--- app/src/main/java/com/example/gurleen/myandroidlab/ChatWindow.java
+++ app/src/main/java/com/example/gurleen/myandroidlab/ChatWindow.java
@@ -3,13 +3,14 @@ package com.example.gurleen.myandroidlab;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.text.Editable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -17,6 +18,8 @@ public class ChatWindow extends Activity {
 ListView listView;
 EditText editText;
 Button sendButton;
+final ChatAdapter messageAdapter = new ChatAdapter(this);
+
 ArrayList<String> arrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +28,16 @@ ArrayList<String> arrayList;
        listView = findViewById(R.id.listView);
        editText= findViewById(R.id.editTextChatWindow);
        sendButton = findViewById(R.id.sendButton);
-
+       listView.setAdapter(messageAdapter);
         sendButton.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view) {
-                for(int i=0; i< arrayList.size();i++) {
-
-                    //arrayList.add((getText()));
+                String msg = editText.getText().toString();
+                if(!msg.isEmpty()){
+                    arrayList.add(msg);
+                    editText.setText("");
+                    messageAdapter.notifyDataSetChanged();
                 }
 
             }
@@ -51,8 +56,18 @@ ArrayList<String> arrayList;
             return arrayList.get(position);
         }
         public View getView(int position, View convertView, ViewGroup parent){
+            LayoutInflater inflater = ChatWindow.this.getLayoutInflater();
+            View result= null;
+            if(position%2 == 0)
+                result = inflater.inflate(R.layout.chat_row_incoming, null);
+            else
+                result = inflater.inflate(R.layout.chat_row_outgoing, null);
 
-            return convertView;
+            TextView message = (TextView)result.findViewById(R.id.message_text);
+            message.setText(getItem(position));
+            return result;
+
+
         }
 
 
