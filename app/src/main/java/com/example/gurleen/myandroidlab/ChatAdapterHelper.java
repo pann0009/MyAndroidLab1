a0beea6c0f9e313ac0e7a7fa02aef0a71f1c7c09
GurleenPannu <pann0009@algonquinlive.com>
2017-11-20 19:58:24 -0500

    Lab05  completed

---
+++ app/src/main/java/com/example/gurleen/myandroidlab/ChatAdapterHelper.java
@@ -0,0 +1,39 @@
+package com.example.gurleen.myandroidlab;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by Geet on 11/20/2017.
+ */
+
+public class ChatAdapterHelper extends SQLiteOpenHelper {
+    public static final String KEY_ID = "id";
+    public static final String KEY_MESSAGE = "message";
+    public static final String DATABASE_NAME = "Messages";
+    private static final int VERSION_NUM=1;
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.i("QueryCreateTable", "CREATE TABLE " + DATABASE_NAME
+                +  "( " + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                KEY_MESSAGE +" text);");
+        db.execSQL( "CREATE TABLE " + DATABASE_NAME
+                +  "( " + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                KEY_MESSAGE +" text);");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldversion, int newVersion) {
+        db.execSQL("Drop table if exists "+DATABASE_NAME);
+        this.onCreate(db);
+    }
+    public ChatAdapterHelper(Context ctx) {
+        super(ctx, DATABASE_NAME, null, VERSION_NUM);
+    }
+
+} 
+
--- app/src/main/java/com/example/gurleen/myandroidlab/ChatWindow.java
+++ app/src/main/java/com/example/gurleen/myandroidlab/ChatWindow.java
@@ -1,8 +1,12 @@
 package com.example.gurleen.myandroidlab;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,28 +21,47 @@ import com.example.gurleen.myandroidlab.R;
 import java.util.ArrayList;
 
 public class ChatWindow extends Activity {
-ListView listView;
-EditText editText;
-Button sendButton;
-ChatAdapter messageAdapter;
+    ListView listView;
+    EditText editText;
+    Button sendButton;
+    ChatAdapter messageAdapter;
+    ChatAdapterHelper chatAdapterHelper;
+    ArrayList<String> arrayList;
+    SQLiteDatabase db;
+    private final String ACTIVITY_NAME = "ChatWindow";
 
-ArrayList<String> arrayList = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_window);
-       listView = findViewById(R.id.listView);
-       editText= findViewById(R.id.editTextChatWindow);
-       sendButton = findViewById(R.id.sendButton);
+        chatAdapterHelper = new ChatAdapterHelper(this);
+        db = chatAdapterHelper.getWritableDatabase();
+
+        arrayList = new ArrayList<String>();
+        Cursor c = db.rawQuery("select * from "+ ChatAdapterHelper.DATABASE_NAME, new String[]{});
+        Log.i(ACTIVITY_NAME, "Cursor’s  column count =" + c.getColumnCount() );
+        c.moveToFirst();
+        while(!c.isAfterLast() ) {
+            Log.i(ACTIVITY_NAME, "select * from " + ChatAdapterHelper.DATABASE_NAME
+                    + c.getString(c.getColumnIndex(ChatAdapterHelper.KEY_MESSAGE)));
+            arrayList.add(c.getString(c.getColumnIndex(ChatAdapterHelper.KEY_MESSAGE)));
+            c.moveToNext();
+        }
+        listView = findViewById(R.id.listView);
+        editText = findViewById(R.id.editTextChatWindow);
+        sendButton = findViewById(R.id.sendButton);
         messageAdapter = new ChatAdapter(this);
-       listView.setAdapter(messageAdapter);
-        sendButton.setOnClickListener(new View.OnClickListener(){
+        listView.setAdapter(messageAdapter);
+        sendButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 String msg = editText.getText().toString();
-                if(!msg.isEmpty()){
+                if (!msg.isEmpty()) {
                     arrayList.add(msg);
+                    ContentValues cValues = new ContentValues();
+                    cValues.put(ChatAdapterHelper.KEY_MESSAGE, msg);
+                    db.insert(ChatAdapterHelper.DATABASE_NAME, "NullColumnName", cValues);
                     editText.setText("");
                     messageAdapter.notifyDataSetChanged();
                 }
@@ -46,27 +69,30 @@ ArrayList<String> arrayList = new ArrayList<String>();
             }
         });
     }
-    private class ChatAdapter extends ArrayAdapter<String>{
+
+    private class ChatAdapter extends ArrayAdapter<String> {
 
         public ChatAdapter(Context ctx) {
             super(ctx, 0);
         }
-        public int getCount(){
+
+        public int getCount() {
             return arrayList.size();
         }
 
-        public String getItem(int position){
+        public String getItem(int position) {
             return arrayList.get(position);
         }
-        public View getView(int position, View convertView, ViewGroup parent){
+
+        public View getView(int position, View convertView, ViewGroup parent) {
             LayoutInflater inflater = ChatWindow.this.getLayoutInflater();
-            View result= null;
-            if(position%2 == 0)
+            View result = null;
+            if (position % 2 == 0)
                 result = inflater.inflate(R.layout.chat_row_incoming, null);
             else
                 result = inflater.inflate(R.layout.chat_row_outgoing, null);
 
-            TextView message = (TextView)result.findViewById(R.id.message_text);
+            TextView message = (TextView) result.findViewById(R.id.message_text);
             message.setText(getItem(position));
             return result;
 
